9b7071828c28f61ca18468c785e30e9046044548
Zeekerz <pramote.p.on@gmail.com>
2021-02-13 01:52:59 +0700

    test

--- server/config/index.js
+++ server/config/index.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 
 // Exports ข้อมูลที่เราต้องการส่งออกจากใน .env
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    DOMAIN: process.env.DOMAIN
 }
