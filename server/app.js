3c8635e40561126f9e45a02cdd92c5cde66753d4
Zeekerz <pramote.p.on@gmail.com>
2021-02-13 01:34:01 +0700

    middleware errorHandler

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// import error middleware
+const errorHandler = require('./middleware/errorHandler');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -16,5 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// สั่งใช้งาน error middleware ที่เรา Import เข้ามา
+app.use(errorHandler);
 
 module.exports = app;
